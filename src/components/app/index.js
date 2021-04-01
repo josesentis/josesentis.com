ef1be6214f5448edaccbebbfb0a77d476db7d065
Jose Sentis <jose.sentis1987@gmail.com>
2021-04-01 14:25:13 +0200

    Changes email

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { Router } from "react-router-dom";
-import Helmet from 'react-helmet';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import isExplorer from '@runroom/purejs/lib/isExplorer';
@@ -13,7 +12,6 @@ import Cursor from '../cursor';
 import Loader from '../loader';
 
 import { playground } from '../../../package.json';
-import data from '../../data/cache';
 import client from '../../utils/apollo';
 import history from '../../utils/history'
 import GlobalStyle from '../../styles';
@@ -32,26 +30,8 @@ const App = () => {
     firmLogger();
   }, []);
 
-  const title = `${data.name} - ${data.job}`;
-
   return (
     <>
-      <Helmet>
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={data.siteUrl} />
-        <meta property="og:image" content={`${data.siteUrl}/${data.ogImage}`} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={data.description} />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@josesentis_" />
-        <meta name="twitter:title" content={title} data-react-helmet="true" />
-        <meta name="twitter:description" content={data.description} />
-        <meta
-          name="twitter:image"
-          content={`${data.siteUrl}/${data.ogImage}`}
-        />
-      </Helmet>
       <GlobalStyle />
       {!loading ? (
         <ApolloProvider client={client}>
--- src/data/cache.js
+++ src/data/cache.js
@@ -10,7 +10,7 @@ const data = {
     linkedin: `https://www.linkedin.com/in/jose-sentis`,
     instagram: `https://www.instagram.com/codeplayground_`,
     twitter: `https://twitter.com/josesentis_`,
-    email: 'jose.sentis1987@gmail.com',
+    email: 'sayhi@josesentis.com',
   },
   sections: {
     __typename: 'Sections',
