4499d20c10d698b97af3ad91e4fb674150168f80
Jose Sentis <jose.sentis1987@gmail.com>
2021-04-01 19:11:31 +0200

    While sayhi@ doesn't work leaving temporary jose@

--- src/data/cache.js
+++ src/data/cache.js
@@ -10,7 +10,7 @@ const data = {
     linkedin: `https://www.linkedin.com/in/jose-sentis`,
     instagram: `https://www.instagram.com/josesentis.code/`,
     twitter: `https://twitter.com/josesentis_`,
-    email: 'sayhi@josesentis.com',
+    email: 'jose@josesentis.com',
   },
   sections: {
     __typename: 'Sections',
