c26be52c4c177ff4edf3c6d460168952fc7baafa
Jose Sentis <jose.sentis1987@gmail.com>
2021-04-28 14:26:31 +0200

    Edits log

--- src/utils/firm-logger.js
+++ src/utils/firm-logger.js
@@ -3,20 +3,20 @@ import data from '../data/cache';
 const stylesArray = [
   "background: #191919",
   "color: #3cea43;",
-  "padding: 15px 25px",
+  "padding: 15px 10px",
   "font-size: 12px;"
 ].join(';');
 
 const stylesArray2 = [
   "background: #191919",
   "color: #f5f5f5;",
-  "padding: 15px",
-  "line-height: 25px",
+  "padding: 15px 10px",
+  "line-height: 22px",
   "font-size: 12px;"
 ].join(';');
 
 const firmLogger = () => (
-  console.log(`%c⚫️ josesentis.com ⚫️ %cthanks for visiting my portfolio website. contact for project inquiries:\n- ${data.social.email}\n- ${data.social.linkedin}\n- ${data.social.instagram}`, stylesArray, stylesArray2)
+  console.log(`%cDeveloped by josesentis.com%cThanks for visiting my portfolio website. Contact for project inquiries:\n- ${data.social.email}\n- ${data.social.linkedin}\n- ${data.social.instagram}`, stylesArray, stylesArray2)
 );
 
 export default firmLogger;
